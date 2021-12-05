29c0a9d9868c6426e9eaa5cd7b95543a7eef35cc
Billy Kirk <kirk.william.c@gmail.com>
2021-12-05 12:22:33 -0600

    Adding generic todos route

--- server.ts
+++ server.ts
@@ -1,12 +1,13 @@
-import { json, serve, serveRemote } from "https://deno.land/x/verse/mod.ts";
+import {json, serve, serveRemote} from "https://deno.land/x/verse/mod.ts";
 
 serve(8000, {
-  // you can serve plain text
-  "/hello": () => new Response("Hello from api.billykirk.dev"),
+    // you can serve plain text
+    "/hello": () => new Response("Hello from api.billykirk.dev"),
 
-  // json
-  "/json": () => json({ message: "Hello from api.billykirk.dev" }),
+    // json
+    "/json": () => json({message: "Hello from api.billykirk.dev"}),
 
-  // or a remote resource
-  "/todos/:id": serveRemote("https://jsonplaceholder.typicode.com/todos/:id"),
+    // or a remote resource
+    "/todos": serveRemote("https://jsonplaceholder.typicode.com/todos"),
+    "/todos/:id": serveRemote("https://jsonplaceholder.typicode.com/todos/:id"),
 });
