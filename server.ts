a431602bad318abadf6e28d119403cb8b724e8cf
Billy Kirk <kirk.william.c@gmail.com>
2021-12-05 12:11:16 -0600

    Remove serveStatic references

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import {json, serve, serveStatic, serveRemote} from "https://deno.land/x/verse/mod.ts";
+import {json, serve, serveRemote} from "https://deno.land/x/verse/mod.ts";
 
 serve(8000, {
     // you can serve plain text
@@ -6,13 +6,6 @@ serve(8000, {
 
     // json
     "/json": () => json({message: "Hello from api.billykirk.dev"}),
-
-    // a single file
-    "/": serveStatic("./public/index.html"),
-
-    // a directory of files (browsing to /public will present a directory listing page)
-    // note: must include :filename? at end of the path as below
-    "/public/:filename?": serveStatic("./public"),
 
     // or a remote resource
     "/todos/:id": serveRemote("https://jsonplaceholder.typicode.com/todos/:id"),
