1a47396cfae0440bd4aeb107275ea0da3001e3b3
Billy Kirk <kirk.william.c@gmail.com>
2021-12-05 12:24:21 -0600

    Adding other json remote resources

--- server.ts
+++ server.ts
@@ -8,6 +8,21 @@ serve(8000, {
     "/json": () => json({message: "Hello from api.billykirk.dev"}),
 
     // or a remote resource
+    "/posts": serveRemote("https://jsonplaceholder.typicode.com/posts"),
+    "/posts/:id": serveRemote("https://jsonplaceholder.typicode.com/posts/:id"),
+
+    "/comments": serveRemote("https://jsonplaceholder.typicode.com/comments"),
+    "/comments/:id": serveRemote("https://jsonplaceholder.typicode.com/comments/:id"),
+
+    "/albums": serveRemote("https://jsonplaceholder.typicode.com/albums"),
+    "/albumn/:id": serveRemote("https://jsonplaceholder.typicode.com/albums/:id"),
+
+    "/photos": serveRemote("https://jsonplaceholder.typicode.com/photos"),
+    "/photos/:id": serveRemote("https://jsonplaceholder.typicode.com/photos/:id"),
+
     "/todos": serveRemote("https://jsonplaceholder.typicode.com/todos"),
     "/todos/:id": serveRemote("https://jsonplaceholder.typicode.com/todos/:id"),
+
+    "/users": serveRemote("https://jsonplaceholder.typicode.com/users"),
+    "/users/:id": serveRemote("https://jsonplaceholder.typicode.com/users/:id"),
 });
